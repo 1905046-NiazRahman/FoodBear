1303c83d54f00ca516e892c745936b4172cc8b18
Ajoy Dey <ajoydey00001@gmail.com>
2024-02-09 19:51:49 +0600

    rr

--- frontend/pages/Restaurant/FoodCard_Restaurant.js
+++ frontend/pages/Restaurant/FoodCard_Restaurant.js
@@ -4,7 +4,6 @@ import { Form } from "react-bootstrap";
 export default function (props) {
   const [showConfirmModal, setShowConfirmModal] = useState(false);
 
-  
   const handleDelete = () => {
     setShowConfirmModal(true);
   };
