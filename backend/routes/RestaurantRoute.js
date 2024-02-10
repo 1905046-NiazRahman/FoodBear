12a4116066e7b8ff9934b7096471a518064b7453
Ajoy Dey <ajoydey00001@gmail.com>
2024-02-10 14:04:11 +0600

    qq

--- backend/routes/RestaurantRoute.js
+++ backend/routes/RestaurantRoute.js
@@ -2,7 +2,6 @@ const express = require("express")
 const router = express.Router()
 const controllers= require("../controllers/RestaurantController")
 
-
 router.route("/signup")
         .post(controllers.signupRestaurant)
 router.route("/login")
